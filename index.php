9520c12fc17c6269f34f832345f9bda5fdb3f1c3
bbonugba <162338701+bbonugba@users.noreply.github.com>
2024-06-26 21:11:40 +0100

    Embedded the Video links

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php include('Templates/connect.php')  ?>
 <?php include('Templates/header.php')  ?>
+
+<?php 
+$fetch_query = "SELECT * FROM `binge`";
+$send_fetch_query = mysqli_query($connection, $fetch_query);
+
+$binge = mysqli_fetch_all($send_fetch_query, MYSQLI_ASSOC);
+print_r ($binge)
+?>
+
+
+
 <?php include('Templates/footer.php')  ?>
